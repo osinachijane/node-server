c0596e4b5e54641540e6b7b94c578237ab6e6b2e
Jane <nsiegbunam.osinachijane@gmail.com>
2018-10-25 13:19:50 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//import express
+var express = require('express');
+var hbs = require('hbs');
+
+//assign express to app
+var app = express();
+var port = 3001;
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/friends', (req,res)=> {
+    res.send(     [ {
+            name: 'Newton',
+            age: 22,
+            location: 'Lagos'        
+        },
+        {
+            name: 'Newton',  
+            age: 22,
+            location: 'Lagos'        
+        }
+        ])
+
+});
+
+app.post('/add_friends', (req,res)=> {
+    var foods = [
+        {
+        id: 1,
+        name: jane
+        }
+    ]
+    foods.save();
+    req.send()
+});
+
+
+//port to listen on
+app.listen(port, console.log(`This app is listening on port ${port}`));
